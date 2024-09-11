451d5ad425e0981fc1eb06ad9bf5c16022d5b60c
Gratic <blairodu74@yahoo.fr>
2024-09-11 16:43:31 +0200

    test: made the tests work with new repository pattern implementation

--- tests/TasksAPI.test.ts
+++ tests/TasksAPI.test.ts
@@ -2,22 +2,24 @@ import { describe, expect, it, jest } from "@jest/globals";
 import { Response } from "supertest";
 import request from "supertest";
 import makeApp from "../src/app";
-import { Express } from "express";
-import { Task } from "../src/models/Tasks";
+import { Express, Router } from "express";
+import { InMemoryTaskRepository } from "../src/repository/InMemoryTaskRepository";
+import taskRouter from "../src/controllers/TaskController";
 
 describe("Tasks v1 REST API", () => {
 
     let app: Express;
+    let route: Router;
 
     beforeAll(() => {
-        app = makeApp();
+        route = taskRouter(new InMemoryTaskRepository());
+        app = makeApp(route);
     })
 
     it("List of Tasks on / with GET", async () => {
         const res: Response = await request(app)
         .get("/v1/tasks/")
         .send();
-
 
         expect(res.statusCode == 200).toBeTruthy();
         expect(res.type == "application/json").toBeTruthy();
